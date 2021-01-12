ff1e6b05686d50b74926ca7cf286cc0a280e4822
otterice <josephdang101@gmail.com>
2021-01-12 00:38:44 -0800

    using different tech
    more consistent i hope, changes to test

--- js/algo.js
+++ js/algo.js
@@ -2,6 +2,7 @@
 var selectNum = 0;
 var userCol = []; 
 var userType = [];
+var i = 0;
 
 //creates select lists to select a clothing type
 function createSelect() {
@@ -22,42 +23,6 @@ function createSelect() {
 
 //global variable for checking how many images were put in.
 var j = 0;
-
-window.onload = function(){    
-    //Check File API support
-    if(window.File && window.FileList && window.FileReader)
-    {
-        var filesInput = document.getElementById("files");        
-        filesInput.addEventListener("change", function(event){           
-            var files = event.target.files; //FileList object
-            var output = document.getElementById("result");           
-            for(var i = 0; i< files.length; i++)
-            {
-                var file = files[i];                
-                //Only pics
-                if(!file.type.match('image'))
-                  continue;                
-                var picReader = new FileReader();				                
-                picReader.addEventListener("load",function(event){                    
-                    var picFile = event.target;
-                    var div = document.createElement("div");
-					div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
-					"title='" + picFile.name + "'id='myImg" + j + "'/>";
-                    createSelect();
-					j++;
-                    output.insertBefore(div, null);   
-                                               
-                });                
-                 //Read the image
-                picReader.readAsDataURL(file);       				
-            }           
-        });
-    }
-    else
-    {
-        console.log("Your browser does not support File API");
-    }       
-}
 
 var send = document.getElementById("send");
 
@@ -81,4 +46,48 @@ send.onclick = function(){
         console.log(userCol[i]);
         console.log(userType[i]);
     }
+}
+
+document.getElementById('shirtsFile').onclick = function() {
+      insertImage('shirtsFile', 'result');
+}
+
+document.getElementById('pantsFile').onclick = function() {
+    insertImage('pantsFile', 'result2');
+}
+
+//Inserts the image into the database/lets users see what images are put in.
+function insertImage(type, outputResult) {
+    if(window.File && window.FileList && window.FileReader)
+    {
+        var filesInput = document.getElementById(type);        
+        filesInput.addEventListener("change", function(event){           
+        var files = event.target.files; //FileList object
+        var output = document.getElementById(outputResult);           
+        //for(var i = 0; i < files.length; i++)
+        //{
+            var file = files[i];         
+            //Only pics
+            //if(!file.type.match('image'))
+            //    continue;                
+            var picReader = new FileReader();				                
+            picReader.addEventListener("load",function(event){                    
+                var picFile = event.target;
+                var div = document.createElement("div");
+                //Create new image div
+                div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+                "title='" + picFile.name + "'id='myImg" + j + "'/>";
+                //createSelect();
+                j++;
+                output.insertBefore(div, null);                                               
+            });                
+                //Read the image
+            picReader.readAsDataURL(file);       				
+        //}           
+        });
+    }
+    else
+    {
+        console.log("Your browser does not support File API");
+    }     
 }
