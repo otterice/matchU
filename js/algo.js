b9c885bd634e2952fd1d1273c33d6aaf520a880f
otterice <josephdang101@gmail.com>
2021-01-16 22:40:20 -0800

    fully functional delete button

--- js/algo.js
+++ js/algo.js
@@ -22,13 +22,13 @@ inputLocalFont2.addEventListener("change", previewImages, false);
 function previewImages() {
     var fileList = this.files;
     var anyWindow = window.URL || window.webkitURL;
-    var colorThief = new ColorThief();
+
     for(var i = 0; i < fileList.length; i++) {
         var objectUrl = anyWindow.createObjectURL(fileList[i]);
         //$('#' + imagePreview).append('<img src="' + objectUrl + '"' + "/>");
         $('#' + imagePreview).append("<span class=\"pip\">" +
         "<img class=\"imageThumb\" src=\"" + objectUrl + "\" title=\"" + file.name + "\"/>" +
-        "<br/><span class=\"remove\">Remove image</span>" +
+        "<br/><span class=remove" + index1 + ">Remove image</span>" +
         "</span>");
 
         img.src = objectUrl;
@@ -40,27 +40,39 @@ function previewImages() {
 img.onload = function() {
   var colorThief = new ColorThief;
   var color = colorThief.getColor(img);
-  /*var temp = {};
-  temp['name' + index1] = img.src*/
+  
   if (imagePreview == "imagePreview") {
-    shirtsDict.push({blob: img.src, value: color});
-    //shirtsDict.push(temp);
+    shirtsDict.push({type: 'shirt', blob: img.src, value:color});
     console.log(shirtsDict);
   }
   else if (imagePreview == "imagePreviewPants") {
-    pantsDict.push({blob: img.src, value:color});
+    pantsDict.push({type: 'pants', blob: img.src, value:color});
     console.log(pantsDict);
   }
+}        
 
-  $(".remove").click(function(){
-    $(this).parent(".pip").remove();
-    //delete shirtsDict['name0'];
-  });
-  
-  index1++;
-  img.src = "";
-}
+//removes the index in shirtsdict
+$(document).on("click", ".remove1", function() {
+  //this doesnt work because there are two different pips
+  //each elem that goes into each box goes from 0,1,2
+  //the other box has its own unique values
+  $el = $(this).parent(".pip");
+  shirtsDict.splice($el.index(), 1);
+  console.log(shirtsDict);
+  $el.remove();
+  $(this).parent(".pip").remove();
+});
 
-function changeID(imagePreview) {
+//removes the index in pantsdict
+$(document).on("click", ".remove2", function() {
+  $el = $(this).parent(".pip");
+  pantsDict.splice($el.index(), 1);
+  console.log(pantsDict);
+  $el.remove();
+  $(this).parent(".pip").remove();
+});
+
+function changeID(imagePreview, index1) {
     this.imagePreview = imagePreview;
+    this.index1 = index1;
 }
